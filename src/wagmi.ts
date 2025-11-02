3a6ef2d47b505c9d656df0753c91fc827f1baceb
Wonhee Lee <2wheeh@gmail.com>
2025-11-02 20:04:00 +0900

    fixup

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,6 +1,6 @@
-import { createConfig, http } from 'wagmi'
-import { mainnet, sepolia } from 'wagmi/chains'
-import { baseAccount, injected, walletConnect } from 'wagmi/connectors'
+import { createConfig, http } from 'wagmi';
+import { mainnet, sepolia } from 'wagmi/chains';
+import { baseAccount, injected, walletConnect } from 'wagmi/connectors';
 
 export const config = createConfig({
   chains: [mainnet, sepolia],
@@ -13,7 +13,7 @@ export const config = createConfig({
     [mainnet.id]: http(),
     [sepolia.id]: http(),
   },
-})
+});
 
 declare module 'wagmi' {
   interface Register {
